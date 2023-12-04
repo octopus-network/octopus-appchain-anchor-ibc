f65da0a1e239664200ab1f3b9b15d446c19155d6
Rivers Yang <rivers.yang@icloud.com>
2023-12-04 12:50:33 +0800

    Fix bugs.

--- appchain-anchor-ibc/src/contract_actions/reward_token_callbacks.rs
+++ appchain-anchor-ibc/src/contract_actions/reward_token_callbacks.rs
@@ -14,11 +14,19 @@ impl RewardTokenCallbacks for AppchainAnchor {
         near_sdk::assert_self();
         match env::promise_result(0) {
             PromiseResult::NotReady => unreachable!(),
-            PromiseResult::Successful(_) => {
-                let anchor_settings = self.anchor_settings.get().unwrap();
-                self.locked_reward_token_amount -= anchor_settings.era_reward.0;
-                let max_gas = Gas::ONE_TERA.mul(10);
-                self.pending_rewards.remove_first(max_gas);
+            PromiseResult::Successful(bytes) => {
+                let amount: U128 = near_sdk::serde_json::from_slice(&bytes).unwrap();
+                if amount.0 == 0 {
+                    let anchor_settings = self.anchor_settings.get().unwrap();
+                    self.locked_reward_token_amount -= anchor_settings.era_reward.0;
+                    let max_gas = Gas::ONE_TERA.mul(10);
+                    self.pending_rewards.remove_first(max_gas);
+                } else {
+                    log!(
+                        "Reward tokens are rejected by LPOS market contract for {:?}.",
+                        deposit_msg
+                    )
+                }
             }
             PromiseResult::Failed => {
                 log!(
